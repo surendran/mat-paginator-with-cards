d0b929b52993bd4c3da51f9f44c50c0fb0030a56
surendran <surendr.88@gmail.com>
2022-05-16 18:30:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mat-paginator-with-cards-mnd9aj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/mat-paginator-with-cards-mnd9aj)
---
+++ app/app.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+
+export interface Card {
+  title: string;
+  subtitle: string;
+  text: string;
+}
+
+const DATA: Card[] = [
+  {
+    title: 'Shiba Inu 1',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 2',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 3',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 4',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 5',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 6',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 7',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 8',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 9',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  },
+  {
+    title: 'Shiba Inu 10',
+    subtitle: 'Dog Breed',
+    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+  }
+];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  obs: Observable<any>;
+  dataSource: MatTableDataSource<Card> = new MatTableDataSource<Card>(DATA);
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+    this.changeDetectorRef.detectChanges();
+    this.dataSource.paginator = this.paginator;
+    this.obs = this.dataSource.connect();
+  }
+
+  ngOnDestroy() {
+    if (this.dataSource) { 
+      this.dataSource.disconnect(); 
+    }
+  }
+}
